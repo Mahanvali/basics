36be25f80ab979c71ec06b94433d53fac959ae3a
Mahanvali <139391502+Mahanvali@users.noreply.github.com>
2024-07-15 13:17:45 +0330

    Switch statements

---
+++ src/Switches.java
@@ -0,0 +1,32 @@
+package src;
+//  Learning Java for IB, Coming from a C++
+//  Switch statements
+public class Switches {
+    public static void main(String args[]){
+        int day = 4;    //  Set day
+        //  Using Iranian standard date timing
+        switch(day){
+            case 1:
+                System.out.println("Saturaday");
+            break;
+            case 2:
+                System.out.println("Sunday");
+            break;
+            case 3:
+                System.out.println("Monday");
+            break;
+            case 4:
+                System.out.println("Tuesday");
+            break;
+            case 5:
+                System.out.println("Wednesday");
+            break;
+            case 6:
+                System.out.println("Thursday");
+            break;
+            case 7:
+                System.out.println("Friday");
+            break;
+        }
+    }
+}
